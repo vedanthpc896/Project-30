47113372cd0d780152070219e3dbd34b90b2f572
Programmer_King <72929704+vedanthpc896@users.noreply.github.com>
2021-01-13 19:58:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# p5.play-boilerplate
-Boiler plate for p5.play
+
+### Features of Stage 3:
+##### On Space button pressed, player gets a second chance to play
+##### Score card is calculated 
+##### Blocks vanish on coming in contact with my slingshot
+
+
+
+
+
--- sketch.js
+++ sketch.js
@@ -3,88 +3,128 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-
-
 var engine, world;
+var holder,polygon,ground;
 var stand1,stand2;
-var b1,b2,b3,b4,b5,b6,b7,b8,b9;
-var slingshot;
-var ground;
-var polygon, polygon_img;
-
-
+var polygon;
+var slingShot;
+var polygon_img;
 function preload(){
   polygon_img=loadImage("polygon.png");
 }
-
-
 function setup() {
-  createCanvas(800,800);
+  createCanvas(900,400);
   engine = Engine.create();
-  world = engine.world; 
-  
-  ground = new Ground(300,780,800,20)
-  stand1 = new Ground(300,700,400,20);
-  stand2 = new Ground(500,400,400,20);
-//level two boxes
-  b1 = new Box(330,235,30,40);
-  b2 = new Box(360,235,30,40);
-  b3 = new Box(390,235,30,40);
-  b4 = new Box(420,235,30,40);
-  b5 = new Box(450,235,30,40);
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground();
+  stand1 = new Stand(390,300,250,10);
+  stand2 = new Stand(700,200,200,10);
+ 
+  //level one
+  block1 = new Block(300,275,30,40);
+  block2 = new Block(330,275,30,40);
+  block3 = new Block(360,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(420,275,30,40);
+  block6 = new Block(450,275,30,40);
+  block7 = new Block(480,275,30,40);
+  //level two
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  //level three
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  //top
+  block16 = new Block(390,155,30,40);
 
-//level three boxes
+  //set 2 for second stand
+  //level one
+  blocks1 = new Block(640,175,30,40);
+  blocks2 = new Block(670,175,30,40);
+  blocks3 = new Block(700,175,30,40);
+  blocks4 = new Block(730,175,30,40);
+  blocks5 = new Block(760,175,30,40);
+  //level two
+  blocks6 = new Block(670,135,30,40);
+  blocks7 = new Block(700,135,30,40);
+  blocks8 = new Block(730,135,30,40);
+  //top
+  blocks9 = new Block(700,95,30,40);
 
-  b6 = new Box(360,195,30,40);
-  b7 = new Box(390,195,30,40);
-  b8 = new Box(420,195,30,40);
-
-   //top level
-
-   b9 = new Box(390,155,30,40);
-
-   polygon = Bodies.circle(50,200,20);
+  //polygon holder with slings
+  polygon = Bodies.circle(50,200,20);
   World.add(world,polygon);
- 
-  slingshot = new SlingShot(this.polygon,{x:250,y:200});
- 
- }
+  
+  slingShot = new Slingshot(this.polygon,{x:100,y:200});
 
- function draw() {
-   
-  background("black");  
-Engine.update(engine);
-
+}
+function draw() {
+  background(56,44,44); 
+ 
+  //Engine.update(engine);
+  text(mouseX + ',' + mouseY, 10, 15);
+  textSize(20);
+  fill("lightyellow");
+  text("Drag the polygon to destroy the blocks",300,30);
+  textSize(10);
+  text("Press Space to get a second Chance to Play!!",650 ,350);
+  ground.display();
   stand1.display();
   stand2.display();
-  ground.display();
-  strokeWeight(3);
-  stroke(10);
-  fill('lightblue');
-  b1.display();
-  b2.display();
-  b3.display();
-  b4.display();
-  strokeWeight(3);
-  fill('pink');
-  b5.display();
-  b6.display();
-  b7.display();
-  b8.display();
-  strokeWeight(3);
-  fill('grey');
-  b9.display();
-  
+  strokeWeight(2);
+  stroke(15);
+  fill("skyblue");
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  fill("pink");
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  fill("turquoise");
+  block13.display();
+  block14.display();
+  block15.display();
+  fill("grey");
+  block16.display();
+  fill("skyblue");
+  blocks1.display();
+  blocks2.display();
+  blocks3.display();
+  blocks4.display();
+  blocks5.display();
+  fill("turquoise");
+  blocks6.display();
+  blocks7.display();
+  blocks8.display();
+  fill("pink")
+  blocks9.display();
+  fill("gold");
   imageMode(CENTER)
-  image(polygon_img ,polygon.position.x,polygon.position.y,60,60);
-  slingshot.display();
-    //drawSprites();
-}
+  image(polygon_img ,polygon.position.x,polygon.position.y,40,40);
 
+  slingShot.display();
+  
+}
 function mouseDragged(){
   Matter.Body.setPosition(this.polygon,{x:mouseX,y:mouseY});
 }
-
 function mouseReleased(){
-  slingshot.fly();
+  slingShot.fly();
+}
+function keyPressed(){
+  if(keyCode === 32){
+      slingShot.attach(this.polygon);
+  }
 }
